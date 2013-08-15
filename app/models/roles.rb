af7f8730e9486a3550324026bbaf4e542ed0c044
Andi Altendorfer <andi@iboard.cc>
2013-08-15 13:22:59 +0200

    Added missing files

---
+++ app/models/roles.rb
@@ -0,0 +1,41 @@
+# Can be used for classes which include Store
+# @example
+#   class User
+#     include Store
+#     include Roles
+#     key_mehtod :name
+#   end
+#
+#   u=User.new name: 'Fritz The Cat', roles: %w(cat kinky)
+#   u.has_role? :kinky #=> true
+#   u.has_role? :white #=> false
+#   u.roles            #=> [ :cat, :kinky ]
+module Roles
+
+  # Default roles
+  ROLES = %i(guest confirmed author editor maintainer admin)
+
+  def self.included base
+    base.class_eval do
+      attribute :roles, [] 
+    end
+    base.send( :extend,  ClassMethods )
+    base.send( :include, InstanceMethods )
+  end
+
+  # Defines class-methods for the including class
+  module ClassMethods
+
+  end
+
+  # Defines instance methods for objects of classes which includs this module
+  module InstanceMethods
+
+    # @param [Symbol|String] role
+    # @return [Boolean] true if attribute roles includes role
+    def has_role? role
+      self.roles.map(&:to_sym).include?(role.to_sym)
+    end
+  end
+
+end
---
+++ spec/models/roles_spec.rb
@@ -0,0 +1,40 @@
+require_relative '../spec_helper'
+
+describe Roles, 'A module to be used in user-classes' do
+
+  before :each do
+    User.delete_all!
+  end
+
+  it 'defines the ROLES-array' do
+    expect( Roles::ROLES ).to be_an(Array)
+  end
+
+  context 'with a dummy class' do
+
+    class BClass
+      include Store
+      include Roles
+    end
+
+    let(:test_class) { new BClass }
+
+    it 'injects a roles-field to the base-class' do
+      bc = BClass.new roles: %w( admin author )
+      expect( bc.respond_to?(:has_role?) ).to be_true
+      expect( bc.roles ).to eq( %w( admin author ) )
+    end
+
+    it '.has_role?(xxx) reports true/false' do
+      bc = BClass.new roles: %w( admin author )
+      expect( bc.has_role?(:admin) ).to be_true
+      expect( bc.has_role?('admin') ).to be_true
+      expect( bc.has_role?(:author) ).to be_true
+      expect( bc.has_role?('author') ).to be_true
+      expect( bc.has_role?(:root) ).to be_false
+      expect( bc.has_role?('root') ).to be_false
+    end
+
+  end
+
+end
