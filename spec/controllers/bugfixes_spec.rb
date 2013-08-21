ddd9f8c0ff287dcafba5c3677e352e25b381af5b
Andi Altendorfer <andi@iboard.cc>
2013-08-21 10:49:00 +0200

    Fixed error when body is blank

---
+++ spec/controllers/bugfixes_spec.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+require_relative '../spec_helper'
+
+describe 'Specs for high-level bugfixes' do
+
+
+  describe PagesController do
+    render_views
+
+    it 'should not crash with markdown if body is nil' do
+      page = Page.create! title: "with empty body"
+      expect{ visit page_path(page) }.not_to raise_error
+    end
+
+  end
+end
+
