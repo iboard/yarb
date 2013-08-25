76de28a2561e4c57c58755d2079b50a57f713dcb
Andi Altendorfer <andi@iboard.cc>
2013-08-25 15:01:10 +0200

    Review model-specs

--- spec/models/nil_user_spec.rb
+++ spec/models/nil_user_spec.rb
@@ -1,28 +1,28 @@
 # -*- encoding : utf-8 -*-
-require_relative '../spec_helper'
+require_relative "../spec_helper"
 
-describe NilUser, 'A replacement for if current_user ....' do
+describe NilUser, "(A replacement for current_user == nil)" do
 
-  let(:user) { NilUser.new( name: 'dont care', email: 'no@where.at.all') }
+  let(:user) { NilUser.new( name: "dont care", email: "no@where.at.all") }
 
-  it 'initialize but throw away arguments' do
+  it "initialize and throws away arguments" do
     expect( user.name ).to eq('(no name)')
     expect( user.email).to be_nil
   end
 
-  it 'has no password' do
+  it "has no password" do
     expect( user.password ).to be_nil
   end
 
-  it 'never authenticates' do
+  it "never authenticates" do
     expect( user.authenticate('whatever')).to be_false
   end
 
-  it 'has no roles' do
+  it "has no roles" do
     expect( user.roles ).to be_empty
   end
 
-  it 'always answers no to has_role?' do
+  it "always answers no to has_role?" do
     expect( user.has_role?(:some) ).to be_false
   end
 
