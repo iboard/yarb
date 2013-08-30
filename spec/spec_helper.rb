9d9acb9c945bcc9e81093f6c9aee50649a7398bb
Andi Altendorfer <andi@iboard.cc>
2013-08-30 11:05:40 +0200

    Reviewed rspec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # -*- encoding : utf-8 -*-
 
+# SETUP THE ENVIRONMENT AND SIMPLE_COV
 ENV["RAILS_ENV"] ||= 'test'
 require 'simplecov'
 SimpleCov.start do
@@ -10,50 +11,20 @@ SimpleCov.start do
   add_group "Library", 'lib/'
 end
 
-
+# REQUIRE RAILS-ENVIRONMENT AND LIBS
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
+
+# LOADING OWN SPEC-SUPPORT FILES
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-
-def pstore_path
-  r = File.join(Rails.root, 'db', Rails.env.to_s)
-end
-
-def sign_in_as user, password
-  visit sign_in_path
-  fill_in 'EMail', with: user
-  fill_in 'Password', with: password
-  click_button 'Sign In'
-end
-
-def sign_out path=nil
-  visit sign_out_path
-  visit path if path
-end
-
-def create_valid_user email, name, password
-  u= User.new email: email, name: name
-  u.password = password
-  u.save
-end
-
-def sign_out_user
-  visit sign_out_path
-end
-
-def page_should_have_notice page, text
-  page.should have_css( '.alert-success', text: text,  match: :prefer_exact )
-end
-
-def page_should_have_error page, text
-  page.should have_css( '.alert-error', text: text,  match: :prefer_exact )
-end
-
+# CONFIGURING RSPEC
 RSpec.configure do |config|
+
+  # CONFIG CAPYBARA
   config.include Capybara::DSL
   Capybara.configure do |cfg|
     cfg.match = :one
@@ -63,10 +34,12 @@ RSpec.configure do |config|
     cfg.javascript_driver = :webkit
   end
 
+  # SETUP RSPEC OPTIONS
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
   config.color_enabled = true
 
+  # MAKE SURE WE START WITH A CLEAN/EMPTY DATA-PATH
   config.before(:all) do
     if Rails.env.test? || Rails.env.cucumber?
       FileUtils.rm_rf(Dir["#{pstore_path}/[^.]*"])
---
+++ spec/support/steps.rb
@@ -0,0 +1,36 @@
+# -*- encoding : utf-8 -*-
+
+def pstore_path
+  r = File.join(Rails.root, 'db', Rails.env.to_s)
+end
+
+def sign_in_as user, password
+  visit sign_in_path
+  fill_in 'EMail', with: user
+  fill_in 'Password', with: password
+  click_button 'Sign In'
+end
+
+def sign_out path=nil
+  visit sign_out_path
+  visit path if path
+end
+
+def create_valid_user email, name, password
+  u= User.new email: email, name: name
+  u.password = password
+  u.save
+end
+
+def sign_out_user
+  visit sign_out_path
+end
+
+def page_should_have_notice page, text
+  page.should have_css( '.alert-success', text: text,  match: :prefer_exact )
+end
+
+def page_should_have_error page, text
+  page.should have_css( '.alert-error', text: text,  match: :prefer_exact )
+end
+
